bc96d3937869c5e3d0a3f6690d04589eef9e3974
Denis Capelete <capelete.denis@gmail.com>
2025-09-04 21:07:20 -0300

    Roteando Página de Componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,20 @@
-import Text from "./components/text";
-import TrashIcon from "./assets/icons/trash.svg?react"
-import CheckIconIcon from "./assets/icons/check.svg?react"
-import PencilIcon from "./assets/icons/pencil.svg?react"
-import PlusIcon from "./assets/icons/plus.svg?react"
-import SpinnerIcon from "./assets/icons/spinner.svg?react"
-import XIcon from "./assets/icons/x.svg?react"
-import Icon from "./components/icon";
-import Badge from "./components/badge";
-import Button from "./components/button";
-import ButtonIcon from "./components/button-icon";
-import InputText from "./components/input-text";
-import InputCheckbox from "./components/input-checkbox";
-import Card from "./components/card";
-import Container from "./components/container";
-import Skeleton from "./components/skeleton";
+import { BrowserRouter, Route, Routes } from "react-router";
+import PageComponents from "./pages/page-components";
+import LayoutMain from "./pages/layout-main";
+import PageHome from "./pages/page-home";
+
 
 export default function App() {
 
   return (
-    <Container>
-
-      <div className="grid gap-6">
-        <div className="flex flex-col gap-2">
-          <Text variant={"body-sm-bold"} className="text-pink-base">
-            Olá mundo!
-          </Text>
-          <Text className="text-green-dark">
-            Olá mundo!
-          </Text>
-          <Text variant={"body-sm-bold"} >
-            Olá mundo!
-          </Text>
-        </div>
-
-        <div className="flex gap-1">
-          <Icon svg={TrashIcon} className="fill-green-base" />
-          <Icon svg={CheckIconIcon} />
-          <Icon svg={PencilIcon} />
-          <Icon svg={PlusIcon} />
-          <Icon svg={SpinnerIcon} animate />
-          <Icon svg={XIcon} />
-        </div>
-
-        <div className="flex gap-1">
-          <Badge variant={"secondary"}>5</Badge>
-          <Badge variant={"primary"}>2 de 5</Badge>
-          <Badge loading>5</Badge>
-
-        </div>
-
-        <div>
-          <Button icon={PlusIcon}>Nova Tarefa</Button>
-        </div>
-
-        <div className="flex gap-1">
-          <ButtonIcon icon={TrashIcon} />
-          <ButtonIcon icon={TrashIcon} variant={'secondary'} />
-          <ButtonIcon icon={TrashIcon} variant={'tertiary'} />
-          <ButtonIcon loading icon={TrashIcon} />
-        </div>
-
-        <div>
-          <InputText />
-        </div>
-
-        <div>
-          <InputCheckbox />
-        </div>
-        <div>
-          <InputCheckbox loading />
-        </div>
-
-        <div>
-          <Card size={'md'}>Olá mundo!</Card>
-        </div>
-
-        <div className="space-y-2">
-          <Skeleton className="h-8" />
-          <Skeleton className="h-8" />
-          <Skeleton className="w-96 h-8" />
-        </div>
-      </div>
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutMain />}>
+          <Route index element={<PageHome />} />
+          <Route path='/componentes' element={<PageComponents />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
+
 }
